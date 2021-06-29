a595beda88328f7feeb67fe078bd4bc66157e078
TianXY-MouF <72534508+TianXY-MouF@users.noreply.github.com>
2021-06-29 16:17:01 +0800

    Add files via upload
    本次提交解决了arctan函数，在收敛域以外，即【-1,1】以外，函数无输出结果，出现错误的BUG

--- arctan_result.m
+++ arctan_result.m
@@ -1,11 +1,11 @@
 function [TaylorExpansion] = arctan_result(x)
 %ARCTAN_RESULT arctan x = x - x^3/3 + x^5/5 - ... 
 %   计算arctan函数的结果
+if(x>=-1&&x <=1)
  negation = 1;%取反
  index = x;   %指数
  Denominator = 1;%阶乘
  TaylorExpansion = x;
-
     while(true)
         
         Denominator = Denominator +2;                 %求分母的值
@@ -19,6 +19,36 @@ function [TaylorExpansion] = arctan_result(x)
             
             
     end
+end
+if(x<-1||x>1)
+ x=1/x;  
+ negation = 1;%取反
+ index = x;   %指数
+ Denominator = 1;%阶乘
+ TaylorExpansion = x;
+    while(true)
+        
+        Denominator = Denominator +2;                 %求分母的值
+        index = index*x*x;                        %求num2的次方
+        negation = -negation;                     %每次循环取反
+        sum = index / Denominator * negation;       %泰勒展开式求和
+        TaylorExpansion = sum+TaylorExpansion;
+        
+        if abs(sum) <=1e-6
+            break;
+        end
+            
+            
+    end
+    if(x>0)
+    TaylorExpansion=pi/2-TaylorExpansion;
+    end
+    if(x<0)
+    TaylorExpansion=-pi/2-TaylorExpansion;
+    end
+        
+end
+    
     
 
 
