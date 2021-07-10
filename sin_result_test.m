e20bd012aff3c628119cb6405a5f6db439159309
haungzhilei630 <86191780+haungzhilei630@users.noreply.github.com>
2021-07-10 11:30:15 +0800

    Add files via upload

--- sin_result_test.m
+++ sin_result_test.m
@@ -17,10 +17,10 @@ end
 figure(1)
 subplot(3,1,1)
 plot(sita,y1);
-title('0-360°不调用sin函数计算值');xlabel('角度/°');ylabel('计算值');
+title('0°-360°不调用sin函数计算值');xlabel('角度/°');ylabel('计算值');
 subplot(3,1,2)
 plot(sita,y2);
-title('0-360°调用sin函数计算值');xlabel('角度/°');ylabel('计算值');
+title('0°-360°调用sin函数计算值');xlabel('角度/°');ylabel('计算值');
 subplot(3,1,3)
 plot(sita,dy);
-title('0-360°不调用sin函数与调用sin函数两者计算误差值');xlabel('角度/°');ylabel('计算误差值');
+title('0°-360°不调用sin函数与调用sin函数两者计算误差值');xlabel('角度/°');ylabel('计算误差值');
